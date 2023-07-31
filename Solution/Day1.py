af7fd0008f2023d63a76d65985a6462223582ea7
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-07-31 23:24:16 +0900

    프로그래머스 코테입문 Day1

---
+++ Solution/Day1.py
@@ -0,0 +1,14 @@
+def solution(num1, num2):
+    answer = num1 + num2
+    return answer
+
+ def solution(num1, num2):
+    return num1-num2
+
+ def solution(num1, num2):
+    answer = num1*num2
+    return answer
+
+ def solution(num1, num2):
+    answer = num1//num2
+    return answer
