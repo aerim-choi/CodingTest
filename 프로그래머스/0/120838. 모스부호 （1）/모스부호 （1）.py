d0e66b817088ccfaf5e834c1de2a639a4b53f101
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-04-11 20:48:59 +0900

    [level 0] Title: 모스부호 (1), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -0,0 +1,18 @@
+def solution(letter):
+    morse_dic = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    
+    morse_letters = letter.split(' ')
+    
+    answer = ''
+    
+    for morse in morse_letters:
+        answer += morse_dic[morse] 
+    
+    
+    return answer
