bb9684f4d4887debbbc86ff4b458bc916573efa5
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-07-24 17:06:00 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
@@ -1,8 +1,3 @@
 def solution(num, k):
-    num_list = list(str(num))
-    k = str(k)
-    if k in num_list:
-        answer = num_list.index(k)+1
-    else:
-        answer=-1
+    answer = 0
     return answer
